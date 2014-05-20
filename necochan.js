ce85d7af4af30c7856ab4f0fdbe7219b7ee7b19e
Masahiro <masahiro.rollin@gmail.com>
2014-05-21 01:16:48 +0900

    Added private neco method in NecochanClass

--- necochan.js
+++ necochan.js
@@ -2,6 +2,8 @@
     function Necochan () {
         this.neco = 'neconeco':
         this.action = 'peropero':
+
+        function privateNeco () {}
     };
 
     Necohcan.prototype.init = function () {};
